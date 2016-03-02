f626a160a1c0e2bd1c3246ff70c289bc3cb0bd06
Felix Fennell <felnne@bas.ac.uk>
2016-03-02 17:28:56 +0000

    Formatting

--- provisioning/scripts/icehack/miniconda-configure-update.sh
+++ provisioning/scripts/icehack/miniconda-configure-update.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash -eux
 
 # Configure and update Miniconda
-cd /home/user
-/home/user/miniconda/bin/conda config --set always_yes yes --set changeps1 no
-/home/user/miniconda/bin/conda update -q conda
-/home/user/miniconda/bin/conda install _license
-/home/user/miniconda/bin/conda info -a
+cd /home/user;
+/home/user/miniconda/bin/conda config --set always_yes yes --set changeps1 no;
+/home/user/miniconda/bin/conda update -q conda;
+/home/user/miniconda/bin/conda install _license;
+/home/user/miniconda/bin/conda info -a;
